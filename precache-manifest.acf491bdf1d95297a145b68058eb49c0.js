38df9ff869133d6eed76ad05df38952c2e670a68
KvX <KvX989@gmail.com>
2020-06-25 17:59:07 +0200

    Updates

---
+++ precache-manifest.acf491bdf1d95297a145b68058eb49c0.js
@@ -0,0 +1,342 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab6d91c780f685cb0897a19f651eb0a0",
+    "url": "/SPA-space/index.html"
+  },
+  {
+    "revision": "2c6f036dc9fbef25d996",
+    "url": "/SPA-space/static/css/main.c445f97d.chunk.css"
+  },
+  {
+    "revision": "a27f18f0e1877416cd1e",
+    "url": "/SPA-space/static/js/2.2bdc3c60.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/SPA-space/static/js/2.2bdc3c60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c6f036dc9fbef25d996",
+    "url": "/SPA-space/static/js/main.e67a5a36.chunk.js"
+  },
+  {
+    "revision": "8cbd1e37bab82d9b862b",
+    "url": "/SPA-space/static/js/runtime-main.be3a7912.js"
+  },
+  {
+    "revision": "05e8516b2cfce076f3ed952c4a02d1df",
+    "url": "/SPA-space/static/media/Spinner.05e8516b.svg"
+  },
+  {
+    "revision": "a3624f3cdf5366f3a9d28b277e39c810",
+    "url": "/SPA-space/static/media/X.a3624f3c.png"
+  },
+  {
+    "revision": "afb36cd3d101b6a707ea0a05c2b132cc",
+    "url": "/SPA-space/static/media/andromeda.afb36cd3.jpg"
+  },
+  {
+    "revision": "e0f9254666a3fa591ac410a8f3c716af",
+    "url": "/SPA-space/static/media/api.e0f92546.png"
+  },
+  {
+    "revision": "08f32f18d7d0310584f15d3b5e53e61e",
+    "url": "/SPA-space/static/media/arrow-up.08f32f18.svg"
+  },
+  {
+    "revision": "649e7a6c01377fbeaa8a8c91a6a80f2c",
+    "url": "/SPA-space/static/media/asteroiid.649e7a6c.jpg"
+  },
+  {
+    "revision": "a474ff42350ecca1f7996e71ea07d0aa",
+    "url": "/SPA-space/static/media/calendar.a474ff42.svg"
+  },
+  {
+    "revision": "f8083bc47b4bf3062d26205345eb8b16",
+    "url": "/SPA-space/static/media/canis-major.f8083bc4.jpg"
+  },
+  {
+    "revision": "0dd36403e35c861256738d9b17c733de",
+    "url": "/SPA-space/static/media/ceres.0dd36403.png"
+  },
+  {
+    "revision": "5bba7d8f82c527bfc8bbe3f4e16c5e53",
+    "url": "/SPA-space/static/media/circle-down.5bba7d8f.svg"
+  },
+  {
+    "revision": "2fd94a9d12cbc5ac989a0539fe8a1ba7",
+    "url": "/SPA-space/static/media/comet.2fd94a9d.jpg"
+  },
+  {
+    "revision": "ac414567bb3b90fcf2cb0a776326ca81",
+    "url": "/SPA-space/static/media/css3.ac414567.svg"
+  },
+  {
+    "revision": "504ae3ba1ff266b016359e11eab5758c",
+    "url": "/SPA-space/static/media/diameter.504ae3ba.svg"
+  },
+  {
+    "revision": "0e0926316cf85e84832b8a2701c8cb3e",
+    "url": "/SPA-space/static/media/earth.0e092631.png"
+  },
+  {
+    "revision": "64ad31ce139e84c0500e933a193bac2b",
+    "url": "/SPA-space/static/media/earth.64ad31ce.svg"
+  },
+  {
+    "revision": "a426cbe21f97f431dfd901e2ff3d049e",
+    "url": "/SPA-space/static/media/envelop.a426cbe2.svg"
+  },
+  {
+    "revision": "081cc59328f451fc43c0285b30a7222c",
+    "url": "/SPA-space/static/media/eris.081cc593.png"
+  },
+  {
+    "revision": "e980bdd4150332da31e893a3001c847f",
+    "url": "/SPA-space/static/media/facebook2.e980bdd4.svg"
+  },
+  {
+    "revision": "b3acb49bd2d8aa17849d25ce7d92f654",
+    "url": "/SPA-space/static/media/fire.b3acb49b.svg"
+  },
+  {
+    "revision": "1fc7ff0b941570453667b57847926e4c",
+    "url": "/SPA-space/static/media/github.1fc7ff0b.svg"
+  },
+  {
+    "revision": "8734c4e70c73dc60ed9eb43ca01e8c09",
+    "url": "/SPA-space/static/media/haumea.8734c4e7.png"
+  },
+  {
+    "revision": "a6092866bc6c7a8d647b95146e18bcca",
+    "url": "/SPA-space/static/media/hearth.a6092866.png"
+  },
+  {
+    "revision": "914755b03d63fb95027eead00c9cfda8",
+    "url": "/SPA-space/static/media/homepage-photo.914755b0.jpg"
+  },
+  {
+    "revision": "f888deb7a91526a1a0ba5e9a75ecc32d",
+    "url": "/SPA-space/static/media/hooks.f888deb7.png"
+  },
+  {
+    "revision": "f2282f94c041c37ef4db028e428e41c1",
+    "url": "/SPA-space/static/media/html.f2282f94.png"
+  },
+  {
+    "revision": "bcacd9504b080e1d9d1391f63e3e871a",
+    "url": "/SPA-space/static/media/ic10.bcacd950.jpg"
+  },
+  {
+    "revision": "29357d551fb8bc83c808e9ea7e5d2ae5",
+    "url": "/SPA-space/static/media/ic1011.29357d55.jpg"
+  },
+  {
+    "revision": "ba109f2e1e29fe54eaa1cd23878d2e6a",
+    "url": "/SPA-space/static/media/img-1.ba109f2e.jpg"
+  },
+  {
+    "revision": "864a3e6d5b0cade83585e9a1d9d01e10",
+    "url": "/SPA-space/static/media/img-10.864a3e6d.jpg"
+  },
+  {
+    "revision": "87671944e55d14706c10f3eb95969f21",
+    "url": "/SPA-space/static/media/img-11.87671944.jpg"
+  },
+  {
+    "revision": "002fcc88d9cab97f8ef58a0613b864ef",
+    "url": "/SPA-space/static/media/img-12.002fcc88.jpg"
+  },
+  {
+    "revision": "758f1fdd21f6fae4c42120f48d527315",
+    "url": "/SPA-space/static/media/img-13.758f1fdd.jpg"
+  },
+  {
+    "revision": "5c1962c5cb3e7d9edb192ea40e482ad0",
+    "url": "/SPA-space/static/media/img-14.5c1962c5.jpg"
+  },
+  {
+    "revision": "3a64970549580337ccd18ed756abd7f5",
+    "url": "/SPA-space/static/media/img-2.3a649705.jpg"
+  },
+  {
+    "revision": "406559590133d4312fde551127f6197a",
+    "url": "/SPA-space/static/media/img-3.40655959.jpg"
+  },
+  {
+    "revision": "dc8becbcb9badb51f1ca83866b10c4cd",
+    "url": "/SPA-space/static/media/img-4.dc8becbc.jpg"
+  },
+  {
+    "revision": "e02d6c4bfff32ce2979c1c608fad2369",
+    "url": "/SPA-space/static/media/img-5.e02d6c4b.jpg"
+  },
+  {
+    "revision": "7f63a90a9c3c6bee8a6bced4aa658736",
+    "url": "/SPA-space/static/media/img-6.7f63a90a.jpg"
+  },
+  {
+    "revision": "1abcd169fc4acac3cc0fa2cc70c4682b",
+    "url": "/SPA-space/static/media/img-7.1abcd169.jpg"
+  },
+  {
+    "revision": "35d5ef3f644d803de509dfea497212dc",
+    "url": "/SPA-space/static/media/img-8.35d5ef3f.jpg"
+  },
+  {
+    "revision": "c057d500fd72706e52f87b057eaeb883",
+    "url": "/SPA-space/static/media/instagram.c057d500.svg"
+  },
+  {
+    "revision": "05bc9ae76270fcc03f441fd815fc0351",
+    "url": "/SPA-space/static/media/jsx.05bc9ae7.png"
+  },
+  {
+    "revision": "52da27cf240d459dfeea7c5c27dab02b",
+    "url": "/SPA-space/static/media/jupiter-homepage.52da27cf.png"
+  },
+  {
+    "revision": "0fced322a963fdff5568979fbe05f0c4",
+    "url": "/SPA-space/static/media/jupiter.0fced322.png"
+  },
+  {
+    "revision": "9108c0c7a9a60b422b991678085839a6",
+    "url": "/SPA-space/static/media/location.9108c0c7.svg"
+  },
+  {
+    "revision": "5a7ceb5810ec051f159ed77729b1c18c",
+    "url": "/SPA-space/static/media/login-background.5a7ceb58.jpeg"
+  },
+  {
+    "revision": "92e223c26ca09da3d0c13fae45c261e0",
+    "url": "/SPA-space/static/media/login-page.92e223c2.jpg"
+  },
+  {
+    "revision": "b2d65a174fd799d8915a27c0696ea46a",
+    "url": "/SPA-space/static/media/logo.b2d65a17.png"
+  },
+  {
+    "revision": "741e616e42f67b90eacb5f898da07e2d",
+    "url": "/SPA-space/static/media/main.741e616e.jpg"
+  },
+  {
+    "revision": "06247b17429aa851735faae034efcf0c",
+    "url": "/SPA-space/static/media/makemake.06247b17.png"
+  },
+  {
+    "revision": "6c996b741bcae11501cefa33d18e0b6a",
+    "url": "/SPA-space/static/media/mars-homepage.6c996b74.png"
+  },
+  {
+    "revision": "249a4759449f5ec04101b0ce27e6c655",
+    "url": "/SPA-space/static/media/mars.249a4759.png"
+  },
+  {
+    "revision": "afe4aa06c05fe3c33fe1e3e94a1c7469",
+    "url": "/SPA-space/static/media/mars.afe4aa06.jpg"
+  },
+  {
+    "revision": "c143c863809ba3ca04ab5c7c9d1fa6fd",
+    "url": "/SPA-space/static/media/me.c143c863.jpg"
+  },
+  {
+    "revision": "19ceb54a7c8f3ecfd600ed582af8e33f",
+    "url": "/SPA-space/static/media/mercury.19ceb54a.png"
+  },
+  {
+    "revision": "dfa7283c0659741284c370b43f4a3595",
+    "url": "/SPA-space/static/media/messier.dfa7283c.jpg"
+  },
+  {
+    "revision": "91147e5967177ae3e0ce9e7de54ddb16",
+    "url": "/SPA-space/static/media/milky-way.91147e59.jpg"
+  },
+  {
+    "revision": "742e7c690094ccf94ea2762114fcc498",
+    "url": "/SPA-space/static/media/mobile.742e7c69.svg"
+  },
+  {
+    "revision": "0422fe1ebd087bc7aa3e38d00d4cbf86",
+    "url": "/SPA-space/static/media/moon-homepage.0422fe1e.png"
+  },
+  {
+    "revision": "6e2cec45faa5430c4f4024f3beed5eef",
+    "url": "/SPA-space/static/media/moon.6e2cec45.png"
+  },
+  {
+    "revision": "cd221e220bda66c576c9f73c3a047689",
+    "url": "/SPA-space/static/media/nav-background.cd221e22.jpg"
+  },
+  {
+    "revision": "1bc3ff6ae18c6d2ef73602bad0880584",
+    "url": "/SPA-space/static/media/neptune-homepage.1bc3ff6a.png"
+  },
+  {
+    "revision": "e70b3fa920ac22d49f65b3fdc75bc2e9",
+    "url": "/SPA-space/static/media/neptune.e70b3fa9.png"
+  },
+  {
+    "revision": "f0b52704ddb093d4863692e55b2fe7c3",
+    "url": "/SPA-space/static/media/objects-background.f0b52704.jpg"
+  },
+  {
+    "revision": "8c4485b791ca49ec16ecce2ef1645713",
+    "url": "/SPA-space/static/media/planets.8c4485b7.jpg"
+  },
+  {
+    "revision": "43cec8690a1b4d071e756f6fa30e1654",
+    "url": "/SPA-space/static/media/pluto.43cec869.png"
+  },
+  {
+    "revision": "ca70d96a25538b2971e5ab173e2e9e9d",
+    "url": "/SPA-space/static/media/react-router.ca70d96a.png"
+  },
+  {
+    "revision": "010accad4691e6a7e615651062952ea6",
+    "url": "/SPA-space/static/media/react.010accad.svg"
+  },
+  {
+    "revision": "2ed706d87c8619212b7a69dd045be8c6",
+    "url": "/SPA-space/static/media/rest.2ed706d8.jpg"
+  },
+  {
+    "revision": "d111724ed8ec9ef8e20b38ba212a5eee",
+    "url": "/SPA-space/static/media/sagitarius.d111724e.jpg"
+  },
+  {
+    "revision": "1f9b0fac327f2655ee52fe7f7940178d",
+    "url": "/SPA-space/static/media/sass.1f9b0fac.svg"
+  },
+  {
+    "revision": "b2fde352fffcef5af1adfd2fb560bb12",
+    "url": "/SPA-space/static/media/saturn.b2fde352.png"
+  },
+  {
+    "revision": "dc29b7b99647032a3b48e8e8985ab721",
+    "url": "/SPA-space/static/media/segue.dc29b7b9.jpg"
+  },
+  {
+    "revision": "5ca40eea5d6e8eed8dc556f1fa7f9908",
+    "url": "/SPA-space/static/media/sombrero.5ca40eea.jpg"
+  },
+  {
+    "revision": "70d4ee46fd3377a39548176234e6930d",
+    "url": "/SPA-space/static/media/spinner10.70d4ee46.svg"
+  },
+  {
+    "revision": "5be7433dadb88f022c5daea8f8630bf9",
+    "url": "/SPA-space/static/media/uranus.5be7433d.png"
+  },
+  {
+    "revision": "37bcf1fdbc51c52fcc7cc7992b775864",
+    "url": "/SPA-space/static/media/venus.37bcf1fd.png"
+  },
+  {
+    "revision": "5dada282f13f00d506c12f176ff16403",
+    "url": "/SPA-space/static/media/whirpool.5dada282.jpg"
+  },
+  {
+    "revision": "87dac93538b7400915ee8a0a1f2c55ff",
+    "url": "/SPA-space/static/media/youtube.87dac935.svg"
+  }
+]);
