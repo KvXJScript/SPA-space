767c6368177da23ce2b7c1064a64f439f1a4a554
KvX <KvX989@gmail.com>
2020-07-12 22:24:39 +0200

    lazy loading at content

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,14 @@
-import React, {lazy, Suspense} from 'react';
+import React from 'react';
 import '../styles/_base.scss';
-// import Homepage from '../layouts/Header';
+import Homepage from '../layouts/Header';
 import {BrowserRouter as Router} from 'react-router-dom';
 
-const Homepage = lazy(()=>import('../layouts/Header'));
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Suspense fallback = {<div>Loading...</div>}>
-          <Homepage/>
-        </Suspense>
+        <Homepage/>
       </div>
     </Router>
   );
--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import '../styles/_homepage.scss';
 import '../styles/_typography.scss'
 import "../styles/_base.scss";
 import Navigation from './Navigation';
-import Content from './Content';
+// import Content from './Content';
 import Footer from './Footer';
+
+const Content = lazy(()=>import('./Content'));
 
 
 const Homepage = () => {
@@ -12,7 +14,9 @@ const Homepage = () => {
       <div className="page">
          <div className="container v-header">
             <Navigation/>
+            <Suspense fallback = {<div>Loading...</div>}>
             <Content/>
+            </Suspense>
             <Footer/>
          </div>
       </div> 
